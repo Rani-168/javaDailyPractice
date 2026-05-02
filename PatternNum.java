2464512f0b9d6a5c567cf6517485c0f8e5198576
Rani <ranibhosale168@gmail.com>
2026-05-02 20:12:54 +0530

    added java print patten  num code

--- PatternNum.java
+++ PatternNum.java
@@ -1,3 +1,18 @@
-public class PatternNum {
+public class PatternNum { 
     
+    public void printPattern(int x) {
+        
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        PatternNum obj = new PatternNum();
+        int x = 5;
+        obj.printPattern(x);   // no need to print return
+    }
 }
