e1d70131c71a266b8d4f1a7b7cd5d39eeabcd02c
Rani <ranibhosale168@gmail.com>
2026-05-02 20:18:33 +0530

    added java print star patten  num code

---
+++ PatternStar.java
@@ -0,0 +1,17 @@
+public class PatternStar {
+
+    public void printPattern(int x) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        PatternStar obj = new PatternStar();
+        int x = 5;
+        obj.printPattern(x);
+    }
+}
