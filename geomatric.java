dade7065d4daaa44eff43e466f747c0b2868ce1f
Rani <ranibhosale168@gmail.com>
2026-05-02 22:00:29 +0530

    added java print geomatric  num code

---
+++ geomatric.java
@@ -0,0 +1,17 @@
+public class geomatric {
+    
+	public static void main(String[] args) {
+		int a= 2;
+    	int n = 6;
+    	int num = 3;
+    	int term = a;
+    	
+    	for(int i = 1; i<=n; i++) {
+    		System.out.print(term +" ");
+    		term = term *num;
+    		
+    		
+    	}  	
+    
+}
+}
