f4f8bc957ff2ca10b2362982e8577fe59ee0e913
Rani <ranibhosale168@gmail.com>
2026-05-06 22:36:39 +0530

    added java print method os sum  num code

---
+++ AddTwoSum.java
@@ -0,0 +1,17 @@
+public class AddTwoSum {
+	public int add(int a, int b) {
+	    return a + b;
+	}
+
+	 public static void main(String[] args) {
+            	int a = 1;
+            	int b = 2;
+            	
+            	AddTwoSum n = new AddTwoSum();
+            		int result = n.add(a, b);
+            		
+            		System.out.print(result);
+            	
+            	
+            }
+}
